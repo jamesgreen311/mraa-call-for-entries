d769c8493c85d4e02de7930c04dbb458ecf55e7a
capn <jamesgreen.311@gmail.com>
2020-11-01 13:49:26 -0500

    Clean Up
    - Submissions Model

--- Models/Submissions.js
+++ Models/Submissions.js
@@ -1,7 +1,3 @@
-// show name will change, must get from config based on show id
-//const sheetName = getShowName(id);
-//const data = ds.getSheetByName(sheetName); 
-//const data = connect().getSheetByName("Data");
 const dataExhibitSheet = connect().getSheetByName("Exhibits");
 const dataCountsSheet = connect().getSheetByName("Exhibitor Upload Counts");
 
@@ -23,51 +19,12 @@ const DataColMap = {
     member: 14,         
     availability: 15,   
     hidden: 16,         
-    timestamp: 17,      
-
-    // calculated fields - this has been moved to pivot table Exhibitor Upload Count
-    emailCount: 18,     // R
-    artistCount: 19,    // S   
-    totalSubmitted: 20  // T
+    timestamp: 17
 }
 
 const CountsRangeMap = {
     eventCounts: "a2:b",
     eventArtistCounts: "c2:e"
-}
-
-// replaced by pivot table Exhibitor Upload Counts and CountsRangeMap
-const DataRangeMap = {
-    emailCount: "r3:r",
-    artistCount: "s3:s",
-    totalSubmitted: "t3",
-    countsByArtist: "r2:s"
-}
-
-// replaced by getTotalByEvent()
-function getTotalArtistSubmitted(evtTitle) {
-    let ws = connect().getSheetByName(sheet);
-    let count = ws.getRange(DataRangeMap.totalSubmitted).getValue();
-    return count;
-}
-
-// replaced by getTotalByEventArtist()
-function getTotalSubmittedByArtist(evtTitle, email) {
-    let ws = connect().getSheetByName(sheet);
-    let data = ws.getRange(DataRangeMap.countsByArtist).getValues();
-    let count = 0;
-
-    for (d of data) {
-        if (d[0] === "") {
-            // stop when we hit the first blank row in the range.
-            break;
-        }
-        if (d[0].toLowerCase() === email.toLowerCase()) {
-            count = d[1];
-            break;
-        }
-    }
-    return count;
 }
 
 function getTotalByEvent(evtTitle) {
