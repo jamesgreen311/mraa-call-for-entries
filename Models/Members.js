500aafbb09f71098921d764652ccecf7615bf138
capn <jamesgreen.311@gmail.com>
2021-06-18 11:07:14 -0400

    Exhibiting Member Email Fix
    - Email column had moved in member database
    - Membership type is no longer case dependent

--- Models/Members.js
+++ Models/Members.js
@@ -4,9 +4,9 @@ const exhibtingMembersEmail = getExhibitingMembers(wsMembers);
 function getExhibitingMembers(wsMembers) {
     //const membersDirectory = SpreadsheetApp.openById(membersDirectoryId).getSheetByName("Member Directory");
     //const membersDirectory = SpreadsheetApp.openById(membersDirectoryId).getSheetByName("Member Directory");
-    let members = wsMembers.getRange(4, 10, wsMembers.getLastRow() - 3, 6).getValues();
+    let members = wsMembers.getRange(3, 1, wsMembers.getLastRow() - 2, 15).getValues();
     let exhibitingMembers = members.filter(function (e) {
-        return e[5] === "exhibiting";
+        return e[14].toLowerCase() === "exhibiting";
     })
     return exhibitingMembers.map(m => m[0]);
 }
