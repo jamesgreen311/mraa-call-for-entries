7936a985cab1528c54b56b189ca2a9f920f41f79
capn <jamesgreen.311@gmail.com>
2021-10-18 16:13:17 -0400

    Refactor member retrieval

--- Models/Members.js
+++ Models/Members.js
@@ -1,23 +1,59 @@
+/**
+ * Map Members spreadsheet column numbers to column headers
+ */
+const MembersColMap = {
+    email: 1,
+    firstName: 2,
+    lastName: 3,
+    status: 5,
+    phone: 11,
+    membership: 13
+}
 const wsMembers = connect(membersDirectoryId).getSheetByName("Member Directory");
-const exhibtingMembersEmail = getExhibitingMembers(wsMembers);
+const memberInfo = getMemberInfo();
+const exhibtingMembers = getExhibitingMembers();
+const activeExhibitingMembers = getActiveExhibitingMembers();
+
 /**
  * @param  {} wsMembers
  * @returns [] array of member emails
  */
-function getExhibitingMembers(wsMembers) {
-    //const membersDirectory = SpreadsheetApp.openById(membersDirectoryId).getSheetByName("Member Directory");
-    //const membersDirectory = SpreadsheetApp.openById(membersDirectoryId).getSheetByName("Member Directory");
-    let members = wsMembers.getRange(3, 1, wsMembers.getLastRow() - 2, 15).getValues();
-    let exhibitingMembers = members.filter(function (e) {
-        return e[14].toLowerCase() === "exhibiting";
+function getExhibitingMembers() {
+    //let allMembers = getMemberInfo();
+    let exhibitingMembers = memberInfo.filter(function (e) {
+        return e[5].toLowerCase() === "exhibiting";
     })
-    return exhibitingMembers.map(m => m[0]);
+    return exhibitingMembers;
+}
+
+function getActiveExhibitingMembers() {
+    //let allMembers = getMemberInfo();
+    let activeExhibitingMembers = memberInfo.filter(function (e) {
+        return e[5].toLowerCase() === "exhibiting" && e[3].toLowerCase() === "active";
+    });
+    return activeExhibitingMembers;  
 }
 
 /**
  * @param string member email
- * @returns [] array of member first name, last name, phone
+ * @returns [] array of member first name, last name, status, phone, membership type
  */
-function getMemberInfo(email) {
-    
+function getMemberInfo() {
+    let lastRow = wsMembers.getLastRow();
+    let firstRow = 3; // Assumes two header rows
+    let memberInfoList = wsMembers.getRangeList(['A'+firstRow+':C'+lastRow, 
+      'E'+firstRow+':E'+lastRow, 
+      'K'+firstRow+':K'+lastRow, 
+      'M'+firstRow+':M'+lastRow]); 
+    let ranges = memberInfoList.getRanges();
+    let colEmailName = ranges[0].getValues();
+    let colStatus = ranges[1].getValues();
+    let colPhone = ranges[2].getValues();
+    let colMembership = ranges[3].getValues();
+    let colCombined = [];
+
+    for (x=0; x<ranges[0].getValues().length; x++) {
+      colCombined.push([...colEmailName[x], ...colStatus[x], ...colPhone[x], ...colMembership[x]]);
+    }
+    return colCombined; 
 }
